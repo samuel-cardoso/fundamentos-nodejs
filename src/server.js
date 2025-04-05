101cff74ca9136cf3cc9a7496b81d33632c4d60f
Samuel <samuelcardosodev@gmail.com>
2025-04-05 11:06:06 -0300

    methods and http status code

--- src/server.js
+++ src/server.js
@@ -7,23 +7,33 @@
 import http from "node:http"; // O "node" foi colocado antes da importação desse módulo, por que ele é interno. Não foi necessário instalá-lo.
 
 // Exemplo:
-// Rota para criar um usuário (nome, email, senha). Através da "request" eu vou conseguir ter acesso a todas informações de quem está chamando nosso servidor. O "response" é o responsável por enviar uma resposta para quem está chamando o nosso servidor.
+// Rota para criar um usuário (nome, email, senha). Através da "request" eu wvou conseguir ter acesso a todas informações de quem está chamando nosso servidor. O "response" é o responsável por enviar uma resposta para quem está chamando o nosso servidor.
+
+const users = [];
 
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/users") {
     // early return
-    return response.end("Listagem de Usuários");
+    return response
+      .setHeader("Content-type", "applicattion/json") // Dizendo o tipo de conteúdo que está sendo retornado. 
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return response.end("Criação de usuário");
+    users.push({
+      id: "1",
+      name: "Jhon Due",
+      email: "jhondoe@example.com",
+    });
+
+    return response.writeHead(201).end();
   }
 
   console.log(method, url);
 
-  return response.end("Hello World");
+  return response.writeHead(404).end();
 });
 
 // localhost: 3333
